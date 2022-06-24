8f87cacbeeffadd2fdccde0afb67c46caaeae39a
hyongti <hyongtiii@gmail.com>
2022-06-24 16:42:01 +0900

    렌더링(4) 가상돔 적용
    applyDiff를 통해 달라진 노드가 있을 때에만 replace

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import getTodos from "./getTodos.js";
 import todosView from "./view/todos.js";
 import counterView from "./view/counter.js";
 import filtersView from "./view/filters.js";
+import applyDiff from "./applyDiff.js";
 
 import registry from "./registry.js";
 
@@ -17,8 +18,17 @@ const state = {
 // 왜 requestAnimationFrame을 기반으로 DOM을 조작할까?
 // requestAnimationFrame vs. setInterval
 // 이벤트 루프의 동작 방식: https://vimeo.com/254947206
-window.requestAnimationFrame(() => {
-  const main = document.querySelector(".todoapp");
-  const newMain = registry.renderRoot(main, state);
-  main.replaceWith(newMain);
-});
+const render = () => {
+  window.requestAnimationFrame(() => {
+    const main = document.querySelector(".todoapp");
+    const newMain = registry.renderRoot(main, state);
+    applyDiff(document.body, main, newMain);
+  });
+};
+
+window.setInterval(() => {
+  state.todos = getTodos();
+  render();
+}, 1000);
+
+render();
