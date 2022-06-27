ebd36841e721ccc92f0a19bb0b9c79335a160a9a
hyongti <hyongtiii@gmail.com>
2022-06-27 15:58:18 +0900

    DOM 이벤트 관리(2) - app.js 추가
    전체 app도 template을 통해 관리하도록 수정

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import getTodos from "./getTodos.js";
 import todosView from "./view/todos.js";
 import counterView from "./view/counter.js";
 import filtersView from "./view/filters.js";
+import appView from "./view/app.js";
 import applyDiff from "./applyDiff.js";
 
 import registry from "./registry.js";
 
+registry.add("app", appView);
 registry.add("todos", todosView);
 registry.add("counter", counterView);
 registry.add("filters", filtersView);
@@ -15,20 +17,12 @@ const state = {
   currentFilter: "All",
 };
 
-// 왜 requestAnimationFrame을 기반으로 DOM을 조작할까?
-// requestAnimationFrame vs. setInterval
-// 이벤트 루프의 동작 방식: https://vimeo.com/254947206
 const render = () => {
   window.requestAnimationFrame(() => {
-    const main = document.querySelector(".todoapp");
+    const main = document.querySelector("#root");
     const newMain = registry.renderRoot(main, state);
     applyDiff(document.body, main, newMain);
   });
 };
-
-window.setInterval(() => {
-  state.todos = getTodos();
-  render();
-}, 1000);
 
 render();
