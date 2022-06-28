6e51e0ebcaa1e5ffc95a63adc6e03bac780b1112
hyongti <hyongtiii@gmail.com>
2022-06-28 22:07:05 +0900

    웹 컴포넌트(1) - 속성을 가진 컴포넌트
    getter와 setter(getAttribute, setAttribute)를 사용해 속성을 업데이트 하기

--- practice/web_components/components/HelloWorld.js
+++ practice/web_components/components/HelloWorld.js
@@ -1,7 +1,22 @@
+const DEFAULT_COLOR = "black";
+
 export default class HelloWorld extends HTMLElement {
+  get color() {
+    return this.getAttribute("color") || DEFAULT_COLOR;
+  }
+
+  set color(value) {
+    this.setAttribute("color", value);
+  }
+
   connectedCallback() {
     window.requestAnimationFrame(() => {
-      this.innerHTML = "<div>Hello World!</div>";
+      const div = document.createElement("div");
+      div.textContent = "Hello World!";
+
+      div.style.color = this.color;
+
+      this.appendChild(div);
     });
   }
 }
